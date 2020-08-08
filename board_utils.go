e9fce6374f14232dd52918485ae1c2a8fc2e7530
Andrew Burgess <andrew@burgess.io>
2020-08-08 12:16:10 -0400

    cleanup

--- board_utils.go
+++ board_utils.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func FormatBoard(game [9]int) string {
-    board := [9]string{}
+func FormatBoard(moves [9]int) string {
+	board := [9]string{}
 
-    for i, move := range game {
-        // zero-value, no move
-        if move == 0 {
-            break
-        }
-        if i % 2 == 0 {
-            board[move - 1] = "x"
-        } else {
-            board[move - 1] = "o"
-        }
-    }
+	for i, move := range moves {
+		// zero-value, no move
+		if move == 0 {
+			break
+		}
+		if i%2 == 0 {
+			board[move-1] = " x "
+		} else {
+			board[move-1] = " o "
+		}
+	}
 
-    return fmt.Sprintf(" %1s | %1s | %1s \n %1s | %1s | %1s \n %1s | %1s | %1s ", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8])
+	return fmt.Sprintf("%3s|%3s|%3s\n%3s|%3s|%3s\n%3s|%3s|%3s", board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8])
 }
