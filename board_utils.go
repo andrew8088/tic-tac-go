c862cd8e419635cd270c9432a022ba0f2430d728
Andrew Burgess <andrew@burgess.io>
2020-08-08 19:04:24 -0400

    find column wins

--- board_utils.go
+++ board_utils.go
@@ -7,9 +7,9 @@ func FormatBoard(moves Moves) string {
 }
 
 func FindWinner(moves Moves) string {
-	// Check rows
 	board := moves.ToBoard()
 
+	// Check rows
 	if board[0] != "" && board[0] == board[1] && board[1] == board[2] {
 		return board[0]
 	}
@@ -20,6 +20,19 @@ func FindWinner(moves Moves) string {
 
 	if board[6] != "" && board[6] == board[7] && board[7] == board[8] {
 		return board[6]
+	}
+
+	// Check columns
+	if board[0] != "" && board[0] == board[3] && board[3] == board[6] {
+		return board[0]
+	}
+
+	if board[1] != "" && board[1] == board[4] && board[4] == board[7] {
+		return board[1]
+	}
+
+	if board[2] != "" && board[2] == board[5] && board[5] == board[8] {
+		return board[2]
 	}
 
 	return ""
--- board_utils_test.go
+++ board_utils_test.go
@@ -29,6 +29,10 @@ func TestCanDetermineTheWinner(t *testing.T) {
 		{MakeMoves(0, 3, 1, 4, 2), "x"},
 		{MakeMoves(0, 3, 1, 4, 6, 5), "o"},
 		{MakeMoves(8, 3, 7, 4, 6), "x"},
+		// columns wins
+		{MakeMoves(0, 1, 3, 4, 6), "x"},
+		{MakeMoves(0, 1, 8, 4, 6, 7), "o"},
+		{MakeMoves(2, 0, 5, 1, 8), "x"},
 	}
 
 	for _, c := range cases {
